9442dad6fe8bc10820dc9f7843d58634c775b330
Hari <Hari@Hari>
2020-08-01 00:16:28 +0530

    Updated

--- Interview/src/Demo/CodingTest.java
+++ Interview/src/Demo/CodingTest.java
@@ -6,12 +6,17 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class CodingTest {
 	
-	public static List<Stock> read (String[] strArr){		
-		List<Stock> stock_list = new ArrayList<Stock>();		
+	public static List<Date> dateList = new LinkedList<Date>();
+	
+	public static Map<String,List<Stock>> read(String[] strArr) throws ParseException{
+		Map<String,List<Stock>> mplst =new HashMap<String,List<Stock>>();		
 		for(int i =0;i<strArr.length;i++){			
 			String delimiter = ",";
 			String line = null;
@@ -23,13 +28,31 @@ public class CodingTest {
 					e.printStackTrace();
 			}
 		  try {
+			  int count=0;			 
+			  line=br.readLine();
 			  while((line = br.readLine()) != null){				
-					String[] content = line.split(delimiter);					
+					String[] content = line.split(delimiter);
+					if(count == 0)
+					{
+						 dateList.add(new SimpleDateFormat("dd-MMM-yyyy").parse(content[10]));
+						  count++;
+					}
+					
 					String[] attributes = {content[0],content[3],content[4],content[10]};
 					Stock stock = createStock(attributes);
+					
 					List<Stock> list_stock = new ArrayList<Stock>();
-					stock_list.add(stock);
-					System.out.println();												
+					if(mplst.containsKey(content[0])){
+						list_stock = mplst.get(content[0]);
+						list_stock.add(stock);
+						mplst.put(content[0],list_stock);
+					}
+					else{
+						list_stock.add(stock);
+						mplst.put(content[0], list_stock);
+					}
+					
+														
 				}
 			  br.close();		
 	} catch (IOException e) {	
@@ -37,55 +60,82 @@ public class CodingTest {
 	}	
 	
 		}	
-		
-	return stock_list;
+			
+	return mplst;
 }
 
-	
 	public static Stock createStock(String[] CSVdata){
 		String symbol = CSVdata[0];
 		String high = CSVdata[1];
 		String low = CSVdata[2];
 		String time_stamp = CSVdata[3];
-		
-		
+				
 		return new Stock(symbol,high,low,time_stamp);
 	}
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws ParseException, IOException {
 		
+			
 		String file1 = "C:/Users/Hari/Desktop/Abi/iConnect/cm29JAN2020bhav.csv";
 		String file2 = "C:/Users/Hari/Desktop/Abi/iConnect/cm30JAN2020bhav.csv";
 		String file3 = "C:/Users/Hari/Desktop/Abi/iConnect/cm31JAN2020bhav.csv";
 		
-		String[] list = {file1,file2,file3};		
-	    CodingTest.read(list);
-	    
-	    List<Stock> stocks = read(list);
-	     
-	   
-	   for(int i=0;i<stocks.size();i++){
-		   
-		   System.out.print(stocks.get(i).getSymbol()+" "+stocks.get(i).getTimeStamp()+" ");
-		   System.out.print(stocks.get(i).getDataRange());
-		   System.out.println();
-	   }
-	  	   	   
-	   File csvFile = new File("C:\\Users\\Hari\\Desktop\\Abi\\iConnect\\output.csv");
-	   PrintWriter out = new PrintWriter(csvFile); 
-	      
-	   for(Stock s :stocks ){
-	   	out.println(s.getSymbol()+" "+s.getTimeStamp()+" "+s.getDataRange());
-	   }
-	   
-	    out.close();
+		String[] list = {file1,file2,file3};	
+		 File csvFile = new File("C:\\Users\\Hari\\Desktop\\Abi\\iConnect\\output.csv"); 
+		 PrintWriter out = new PrintWriter(csvFile); 
+		 out.print("Symbol");
+		 out.print("Time_stamp");
+		 out.print("Data range");
+		 out.print("Running Average");
+		 out.println();
+		 
+		Map<String,List<Stock>> stocksMap = read(list);     
+		Set<String> stockList =stocksMap.keySet();
+		Double runninAvergae = 0.0;
+		List<Date> sortedList = dateList.stream().sorted().collect(Collectors.toList());
+		for(String stockName : stockList )
+		{
+			runninAvergae = 0.0;
+			System.out.println("StockName "+ stockName);
+			
+			List<Stock> lst = stocksMap.get(stockName);
+			for(int j=0;j<sortedList.size();j++)
+			{
+			String dateFormat = new SimpleDateFormat("dd-MMM-yyyy").format(sortedList.get(j));
+			
+			for(int i=0;i<lst.size();i++)
+			{
+				Stock stk= lst.get(i);
+				System.out.println(stk.getTimeStamp()+ "Date "+dateFormat);
+				if(stk.getTimeStamp().equalsIgnoreCase(dateFormat))
+				{
+				
+				  if(j==0)
+				  {
+				  runninAvergae=stk.getDataRange();
+				  System.out.print(runninAvergae);
+				  }
+				  else
+				  {
+					  
+					  runninAvergae =(runninAvergae+stk.getDataRange())/(j+1);
+					
+				  }
+				  out.println(stk.getSymbol()+","+stk.getTimeStamp()+","+stk.getDataRange()+","+ runninAvergae);
+				}
+			}
+			
+			}
+		}
+	  
+	    	   out.close();
 	}
 }
 
 
 class Stock{
 	
-    String symbol;
+     String symbol;
 	 String high;
 	 String low;
 	 String time_stamp;
@@ -106,27 +156,28 @@ class Stock{
 	}
 	
 	public String getSymbol(){
-		return symbol;
+		return this.symbol;
 	}
 	 public String getHigh(){
-		 return high;
+		 return this.high;
 	 }
 	 public String getLow(){
-		 return low;
+		 return this.low;
 	 }
 	 public String getTimeStamp(){
-		 return time_stamp;
+		 return this.time_stamp;
 	 }
 	 
 	 public double getDataRange(){
 		 double value1 =0;
 		 double value2 =0;
 		 try{
-		  value1 = Double.parseDouble(high);
-		  value2 = Double.parseDouble(low);
+		  value1 = Double.parseDouble(this.high);
+		  value2 = Double.parseDouble(this.low);
 		 }
 		 catch(NumberFormatException ex){
 			 System.out.println("Error in converting");
+			 System.out.println(this.high + "High : Low "+this.low );
 		 }
 		 
 		double data_range = value1 - value2;
@@ -137,4 +188,4 @@ class Stock{
 		return this.symbol +" "+this.high+" "+this.low+" "+this.time_stamp+" ";
 		
 	}
-}	
+}
